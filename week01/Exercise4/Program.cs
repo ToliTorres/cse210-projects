698050f1b0234ebcd545524f8f2313e4588873a0
ToliTorres <toliluz@gmail.com>
2026-01-17 15:41:39 -0600

    Processes a list of numbers and displays basic statistics.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,92 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int number = -1;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        // Get numbers from the user
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        // Check if the list has numbers
+        if (numbers.Count == 0)
+        {
+            Console.WriteLine("No numbers were entered.");
+            return;
+        }
+
+        // Compute the sum
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        // Compute the average
+        double average = 0; 
+        
+        if (numbers.Count > 0)
+        {
+            average = (double)sum / numbers.Count;
+        }
+        
+
+        // Find the largest number
+        int largest = numbers[0];
+        foreach (int num in numbers)
+        {
+            if (num > largest)
+            {
+                largest = num;
+            }
+        }
+
+        // Find the smallest positive number
+        int smallestPositive = int.MaxValue;
+        foreach (int num in numbers)
+        {
+            if (num > 0 && num < smallestPositive)
+            {
+                smallestPositive = num;
+            }
+        }
+
+        // Sort the list
+        numbers.Sort();
+
+        // Display result
+        Console.WriteLine($"the sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
+
+        if (smallestPositive != int.MaxValue)
+        {
+            Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+        }
+        else
+        {
+            Console.WriteLine("There is no positive number in the list.");
+        }
+
+        Console.WriteLine("The sorted list is:");
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
