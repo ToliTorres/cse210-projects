5ce599d748a03fe6c034b05eb34b77509b828ed1
ToliTorres <toliluz@gmail.com>
2026-02-03 03:49:43 -0600

    This program simulates a simple product ordering system. The program runs without user interaction and displays all results in the console.

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Address address1 = new Address("123 Main St", "New York", "NY", "USA");
+        Customer customer1 = new Customer("John Smith", address1);
+
+        Order order1 = new Order(customer1);
+        order1.AddProduct(new Product("Notebook", "A101", 3.25, 3));
+        order1.AddProduct(new Product("Pen", "B201", 1.50, 6));
+
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total Price: ${order1.GetTotalPrice()}\n");
+
+        Address address2 = new Address("45 Queen St", "Toronto", "ON", "Canada");
+        Customer customer2 = new Customer("Ryan Gosling", address2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(new Product("Backpack", "C301", 65.00, 1));
+        order2.AddProduct(new Product("Water Bottle", "D401", 12.00, 2));
+
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total Price: ${order2.GetTotalPrice()}");
     }
 }
